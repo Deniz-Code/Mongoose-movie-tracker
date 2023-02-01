47e5dc2de1368c5c782c3f351aa0e92d7a434817
ThiefGang <104874053+ThiefGang@users.noreply.github.com>
2023-02-01 22:28:19 +0200

    add new view for movies

---
+++ controllers/movies.js
@@ -0,0 +1,9 @@
+import { Movie } from "../models/movie.js"
+
+function newMovie(req, res) {
+  res.render("movies/new", {
+    title: "Add Movie",
+  })
+}
+
+export { newMovie as new }
--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,8 @@
-import { Router } from "express";
-// import * as moviesCtrl from "../controllers/movies.js"
-const router = Router();
+import { Router } from "express"
+import * as moviesCtrl from "../controllers/movies.js"
+const router = Router()
 
 /* GET movies listing. */
-router.get("/", function (req, res) {
-  res.send("respond with a resource");
-});
+router.get("/new", moviesCtrl.new)
 
-export { router };
+export { router }
