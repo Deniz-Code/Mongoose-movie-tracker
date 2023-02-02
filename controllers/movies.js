610c47ab681a4f3bb49203f20161d605f33a28a6
ThiefGang <104874053+ThiefGang@users.noreply.github.com>
2023-02-02 05:22:50 +0200

    add movie list with data

--- controllers/movies.js
+++ controllers/movies.js
@@ -6,4 +6,33 @@ function newMovie(req, res) {
   })
 }
 
-export { newMovie as new }
+function create(req, res) {
+  //massage the data
+  //handle checkbox
+  //handle separating the big cast string into smaller ones
+  req.body.nowShowing = !!req.body.nowShowing
+  if (req.body.cast) {
+    req.body.cast = req.body.cast.split(", ")
+  }
+  //use the model to create a movie(using form data in req.body)
+  Movie.create(req.body)
+    .then((movie) => {
+      res.redirect("/movies/new")
+    })
+    .catch((err) => {
+      console.log(err)
+      res.redirect("/movies/new")
+    })
+  //redirect to new movie page
+}
+
+function index(req, res) {
+  Movie.find({}).then((movie) => {
+    res.render("movies/index", {
+      title: "All Movies",
+      movie: movie,
+    })
+  })
+}
+
+export { newMovie as new, create, index }
--- routes/movies.js
+++ routes/movies.js
@@ -2,7 +2,14 @@ import { Router } from "express"
 import * as moviesCtrl from "../controllers/movies.js"
 const router = Router()
 
-/* GET movies listing. */
+/* GET /movies/new */
+router.get("/",moviesCtrl.index)
 router.get("/new", moviesCtrl.new)
 
+
+
+// POST /movies
+router.post("/",moviesCtrl.create)
 export { router }
+
+
